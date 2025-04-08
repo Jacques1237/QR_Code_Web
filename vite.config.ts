3476a9e6f7985139583ef2b55bcfe574dee376fa
Jacques Cilliers <www.jacques777@gmail.com>
2025-04-08 13:06:52 +0200

    added dist

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/QR_Code_Web/', // 👈 This is the key line for GitHub Pages
+  base: '/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
